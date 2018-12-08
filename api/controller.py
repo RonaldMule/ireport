74fa3d095ad9a7f1889ffb6634810631beee4571
https://github.com/RonaldMule/StoreManager <ronaldmulyowa@gmail.com>
2018-12-08 13:33:26 +0300

    Refactoring code

--- api/controller.py
+++ api/controller.py
@@ -1,12 +1,14 @@
 from flask import request, jsonify, json
 from api.incident_model import Incident, IncidentDb, Base_Incident
 from api.utilities import IncidentValidator
-incident_list = IncidentDb()
+
 class IncidentController():
+    ''' A class for connecting the views with the models '''
     def __init__(self):
-        pass
+        self.conn_db = IncidentDb()
   
     def create_incident(self):
+        '''  A method to create an incidence '''
         data = request.get_json()
         createdBy = data.get("createdBy")
         createdOn = data.get("created_on")
@@ -17,9 +19,8 @@ class IncidentController():
         videos = data.get("videos")
         comment = data.get("comment")
       
-        #validation of user input
+        '''validation of user input fields '''
     
-       
         if not  IncidentValidator.validate_status(status):
             return jsonify ({'status': 400,
                             'error': 'The status provided is not defined'
@@ -33,7 +34,7 @@ class IncidentController():
         incident = Incident(Base_Incident(createdOn, createdBy, 
          flag_type, location), status, images, videos, comment)
         
-        incident_list.add_incident(incident)
+        self.conn_db.add_incident(incident)
      
         return jsonify({
             "status": 201,
@@ -45,19 +46,21 @@ class IncidentController():
     def get_mall_incidents(self):
         '''geting all incidents '''
         return jsonify ({'status':200, 
-                        'data':incident_list.get_incident_json()
+                        'data':self.conn_db.get_incident_json()
         }),200
     
     def get_a_specific_incident(self,incident_id ):
-        response = incident_list.get_incident_by_id(incident_id)
+        ''' A method to get an incidnt by id'''
+        response = self.conn_db.get_incident_by_id(incident_id)
         if response:
             return jsonify({'data':response})
         return jsonify({'message': 'No incident found  please'}) 
 
     def delete_a_specific_incident(self, incident_id):
-        response = incident_list.get_incident_by_id(incident_id)
+        ''' A method to delete a specific incidncence '''
+        response = self.conn_db.get_incident_by_id(incident_id)
         if response:
-            incident_list.incident_list.remove(response)
+            self.conn_db.incident_list.remove(response)
             return jsonify ({'status': 200, 
 
                           'data': [{'incident':incident_id,  
@@ -69,11 +72,12 @@ class IncidentController():
             }), 200
 
     def update_location(self, incident_id):
-        response = incident_list.get_incident_by_id(incident_id)
+        ''' A method for updating the location of an incidence '''
+        response = self.conn_db.get_incident_by_id(incident_id)
         if response:
             data = request.get_json()
             if response['status'] == 'Draft':
-                incident_list.get_incident_by_id(incident_id).update(location = data['location'])
+                self.conn_db.get_incident_by_id(incident_id).update(location = data['location'])
                 return jsonify({ 'status': 201,
                     'data': [{ "incident_id": incident_id,
                                 "message":  'Updated red-flag record\'s location'  
@@ -86,20 +90,23 @@ class IncidentController():
             'error':" no incidents currently"})
 
     def update_comment(self, incident_id):
-        response = incident_list.get_incident_by_id(incident_id)
+        ''' Amethod to update the comment of an incident if the status is Draft '''
+        response = self.conn_db.get_incident_by_id(incident_id)
         if response:
             data = request.get_json(force=True)
             if response['status'] == 'Draft':
-                incident_list.get_incident_by_id(incident_id).update(comment = data['comment'])
+                self.conn_db.get_incident_by_id(incident_id).update(comment = data['comment'])
                 return jsonify({ "status": 400,
                         "data":[{"incident_id":incident_id,
                             'message': 'comment successfully updated'}]
                     })
-            #print(response)
+            
             return jsonify({'status': 400,
                     'error': 'you can not update that please'})  
         return jsonify({'status':400,
             'error':' no incidents currently'}) 
+
+#help(IncidentController)        
 
     
     
