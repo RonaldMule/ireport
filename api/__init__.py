5111b37002f38869e72ca0c0089cbbf5ba739c10
https://github.com/RonaldMule/StoreManager <ronaldmulyowa@gmail.com>
2019-01-11 13:01:52 +0300

    Added user signup and login features[#finishes #163142133]

--- api/__init__.py
+++ api/__init__.py
@@ -6,9 +6,7 @@ from config import  config
 def create_app(config_name):
     '''creating the app instance '''
     app = Flask(__name__, )
-    app.config['JWT_SECRET_KEY'] = 'wh)0**yp@ass+wh3nnot^passis~@!!provided'
-    app.secret_key = 'Z4X*N)_P%***~md9ag01a'
-    #jwt = JWTManager(app)
+   
 
     #blueprints for url
     app.register_blueprint(incident_v1, url_prefix='/api/v1') 
